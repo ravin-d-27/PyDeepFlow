ecdccb5268c936c3c4946ef758ae3ab5f8f6d121
Ravin D <ravin.d3107@outlook.com>
2024-10-11 00:01:28 +0530

    Final Modification of __init__ again

--- pydeepflow/__init__.py
+++ pydeepflow/__init__.py
@@ -5,5 +5,6 @@ from .device import Device
 from .model import Multi_Layer_ANN
 from .learning_rate_scheduler import LearningRateScheduler
 from .checkpoints import ModelCheckpoint
+from .regularization import Regularization
 
-__all__ = ["activation", "activation_derivative", "get_loss_function", "get_loss_derivative", "Device", "Multi_Layer_ANN", "LearningRateScheduler", "ModelCheckpoint"]
+__all__ = ["activation", "activation_derivative", "get_loss_function", "get_loss_derivative", "Device", "Multi_Layer_ANN", "LearningRateScheduler", "ModelCheckpoint", "Regularization"]
