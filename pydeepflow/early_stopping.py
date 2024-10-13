f91a093f3a37f9eddd62a5e820ac4c94babf6f70
pear <pearguacamole40zr@gmail.com>
2024-10-13 21:49:27 +0530

    feat: add option to toggle verbosity during training, add docstring for early stopping

--- pydeepflow/early_stopping.py
+++ pydeepflow/early_stopping.py
@@ -1,11 +1,25 @@
 class EarlyStopping:
-    def __init__(self, patience:int = 5, delta:int = 0) -> None:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, patience:int = 5, delta:float = 0) -> None:
+        """
+        Initializes the EarlyStopping.
+        
+        :param patience (int): How long to wait after last time validation loss improved.
+                               Default: 5
+        :param delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                              Default: 0
+        """
         self.patience = patience
         self.delta = delta
         self.early_stop = False
         self.best_score = None
         self.counter = 0
     def __call__(self, val_loss:float) -> None:
+        """
+        Checks if the condition for early stopping are met and updates early_stop flag
+        
+        :param val_loss (float): The current validation loss.
+        """
         score = - val_loss
         if self.best_score is None:
             self.best_score = score
