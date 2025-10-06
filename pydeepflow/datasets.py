ab1051ac03bf6f42247e88ee4f35308bf590d6f4
BhagwatiOracle <bhagwati101x@gmail.com>
2025-10-06 11:56:51 +0530

    modiflied datasets.py

--- pydeepflow/datasets.py
+++ pydeepflow/datasets.py
@@ -1,7 +1,22 @@
 import pandas as pd
 from sklearn.utils import Bunch 
 
+
 def load_dataset(filename,descr="PyDeepFlow Datatset"):
+    """
+    
+    Load dataset from CSV file.
+
+    Parameters
+    ----------
+    filename (str): Path to the CSV file.
+    descr (str): Description of the dataset.
+
+    Returns
+    ----------
+    Bunch: A Bunch object containing the dataset information.
+    
+    """
     df = pd.read_csv(filename)
 
     x = df.iloc[:,:-1]
@@ -19,6 +34,20 @@ def load_dataset(filename,descr="PyDeepFlow Datatset"):
 
 
 def load_iris(return_X_y=False,as_frame=False):
+    """
+    Load the Iris dataset.
+
+    Parameters
+    ----------
+    return_X_y (bool): If True, returns (data, target) instead of a Bunch object.
+    as_frame (bool): If True, returns a pandas DataFrame instead of a Bunch object.
+
+    Returns
+    ----------
+    Bunch or (data, target) or DataFrame.
+    
+    """
+
     bunch = load_dataset("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data",descr="Iris Dataset")
 
     if return_X_y:
@@ -29,6 +58,19 @@ def load_iris(return_X_y=False,as_frame=False):
 
 
 def load_wine(return_X_y=False,as_frame=False):
+    """
+    Load the Wine Quality dataset.
+
+    Parameters
+    ----------
+    return_X_y (bool): If True, returns (data, target) instead of a Bunch object.
+    as_frame (bool): If True, returns a pandas DataFrame instead of a Bunch object.
+
+    Returns
+    ----------
+    Bunch or (data, target) or DataFrame.
+    """
+
     bunch = load_dataset("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv",descr="Wine Quality Dataset")
 
     if return_X_y:
@@ -37,7 +79,21 @@ def load_wine(return_X_y=False,as_frame=False):
         return bunch.frame
     return bunch
 
+
 def load_digits(return_X_y=False,as_frame=False):
+    """
+    Load the Optical Recognition of Handwritten Digits dataset.
+
+    Parameters
+    ----------
+    return_X_y (bool): If True, returns (data, target) instead of a Bunch object.
+    as_frame (bool): If True, returns a pandas DataFrame instead of a Bunch
+
+    Returns
+    ----------
+    Bunch or (data, target) or DataFrame.
+    """
+
     bunch = load_dataset("https://archive.ics.uci.edu/ml/machine-learning-databases/optdigits/optdigits.tra",descr="Optical Recognition of Handwritten Digits Dataset")
 
     if return_X_y:
